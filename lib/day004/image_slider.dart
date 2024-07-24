34ba864f27b7bfb76bf9d4a9da9032ec716b0ab0
Salma Maarouf <68617759+SalmaW@users.noreply.github.com>
2024-07-24 18:56:50 +0300

    add controller

--- lib/day004/image_slider.dart
+++ lib/day004/image_slider.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 class ImageSlider extends StatefulWidget {
   final List<String> imgList;
 
-  const ImageSlider({required this.imgList, super.key});
+  const ImageSlider({super.key, required this.imgList});
 
   @override
   _ImageSliderState createState() => _ImageSliderState();
@@ -12,6 +12,7 @@ class ImageSlider extends StatefulWidget {
 
 class _ImageSliderState extends State<ImageSlider> {
   int _currentIndex = 0;
+  final CarouselController _carouselController = CarouselController();
 
   List<Widget> _generateImageTiles() {
     return widget.imgList
@@ -34,6 +35,7 @@ class _ImageSliderState extends State<ImageSlider> {
       children: <Widget>[
         CarouselSlider(
           items: _generateImageTiles(),
+          carouselController: _carouselController,
           options: CarouselOptions(
             autoPlay: true,
             enlargeCenterPage: true,
@@ -47,18 +49,20 @@ class _ImageSliderState extends State<ImageSlider> {
         ),
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: widget.imgList.map((url) {
-            int index = widget.imgList.indexOf(url);
-            return Container(
-              width: 8.0,
-              height: 8.0,
-              margin:
-                  const EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: _currentIndex == index
-                    ? const Color.fromRGBO(0, 0, 0, 0.9)
-                    : const Color.fromRGBO(0, 0, 0, 0.4),
+          children: widget.imgList.asMap().entries.map((entry) {
+            return GestureDetector(
+              onTap: () => _carouselController.animateToPage(entry.key),
+              child: Container(
+                width: 8.0,
+                height: 8.0,
+                margin:
+                    const EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: _currentIndex == entry.key
+                      ? const Color.fromRGBO(0, 0, 0, 0.9)
+                      : const Color.fromRGBO(0, 0, 0, 0.4),
+                ),
               ),
             );
           }).toList(),
