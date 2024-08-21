3270f715c1e6dfabf00467e1b1d2e5a533fd9151
Salma Maarouf <68617759+SalmaW@users.noreply.github.com>
2024-08-21 15:05:08 +0300

    day12

--- lib/day011/task/main.dart
+++ lib/day011/task/main.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-import 'day010/task/pages/home/home_page.dart';
+import '../day010/task/pages/home/home_page.dart';
 
 void main() {
   runApp(const MyApp());
